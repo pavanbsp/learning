b56a3b36cf41f0ce6fe83ce8906558c683c0f0f1
Pavan <pavanboina6@gmail.com>
2022-12-16 11:28:39 +0530

    initial

---
+++ src/main/java/com/increff/employee/EmployeeApi.java
@@ -0,0 +1,56 @@
+package com.increff.employee;
+
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class EmployeeApi {
+    private static final Logger logger = Logger.getLogger(EmployeeApi.class);
+    private Connection con;
+    public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException {
+    }
+
+    public EmployeeApi() throws Exception {
+        Properties props = new Properties();
+        InputStream inStream = new FileInputStream("employee.properties");
+        props.load(inStream);
+
+        Class.forName(props.getProperty("jdbc.driver"));
+        con = DriverManager.getConnection(props.getProperty("jdbc.url"), props.getProperty("jdbc.username"),
+                props.getProperty("jdbc.password"));
+
+    }
+
+    public ResultSet selecting() throws SQLException{
+        logger.warn("selecting employee");
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery("select * from employee");
+        return rs;
+//        while(rs.next()) {
+//            logger.info(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+//        }
+//        stmt.close();
+    }
+
+    public void inserting(int id, String name, int age) throws SQLException{
+        Statement stmt=con.createStatement();
+        stmt.executeUpdate("insert into employee values(" + id + ", " + '"' +  name + '"' + ", " + age + ")");
+        stmt.close();
+    }
+
+    public void deleting(String row, String val) throws SQLException{
+        Statement stmt=con.createStatement();
+        stmt.executeUpdate("delete from employee where " + row + " = " + val);
+        stmt.close();
+    }
+}
+
+
---
+++ src/main/java/com/increff/employee/EmployeeTest.java
@@ -0,0 +1,24 @@
+package com.increff.employee;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.ResultSet;
+
+import com.increff.employee.EmployeeApi;
+import org.junit.Test;
+
+public class EmployeeTest {
+
+    @Test
+    public void sayHello() throws Exception {
+        EmployeeApi api = new EmployeeApi();
+        //api.deleting("id", "14");
+        //api.inserting(14, "sujatha", 70);
+        ResultSet rs = api.selecting();
+        int i = 0;
+        while (rs.next()) {
+            i++;
+        }
+        assertEquals(6, i);
+    }
+}
